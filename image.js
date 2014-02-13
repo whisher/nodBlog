8304c051dec06b8878cee65785bbece0f5e66ab0
Fabio Bedini <whisher06@gmail.com>
2014-02-13 12:20:58 +0100

    first step refactoring deploy server

--- image.js
+++
@@ -1,9 +0,0 @@
-var im = require('imagemagick');
-im.resize({
-  srcPath: './me2.jpg',
-  dstPath: './me2-1.jpg',
-  width:   256
-}, function(err, stdout, stderr){
-  if (err) throw err;
-  console.log('resized kittens.jpg to fit within 256x256px');
-});
